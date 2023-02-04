7599fa80a80316173a05a97cc8ccaca86c4b6792
Gavin <gavinl3@uci.edu>
2023-02-04 12:59:39 -0800

    gavin commit

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+import http.client
+def foo():
+    conn = http.client.HTTPSConnection("api.github.com")
+
+    payload = ""
+
+    dataGit = conn.request("GET", "/users/divamkumar/repos", payload)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    #print(data.decode("utf-8"))
+    return dataGit.json()
+
+print(foo())
