4bcb44c3c4897bd9ae4874d5d232be0cad56e286
skonaka6 <78990481+skonaka6@users.noreply.github.com>
2023-02-04 23:55:10 -0800

    Added documentation to ServerSetup.py

--- backend/ServerSetup.py
+++ backend/ServerSetup.py
@@ -1,13 +1,15 @@
+#Run this file to set up the cockroach database and tables
+# uses Sean's cockroach account I think
+
 import DBfunctions as DBfuncs
 
 if __name__=="__main__":
     print("Running ServerSetup.py")
     print("Running set_database()...")
     DBfuncs.set_database()
-
     print("Running create_tables()...")
     DBfuncs.create_tables()
+    print("Finished setup.")
 
-    print("Finished.")
 
     DBfuncs.print_database_logistics()
